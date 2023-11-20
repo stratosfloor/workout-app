c030ec7a90b3b324e435c9023ea2f9931963e324
Emil Noren <stratosfloor@hotmail.com>
2023-11-20 16:58:31 +0100

    Implemente basic workout exercise-view

--- workout_app/lib/views/exercise/exercise_view.dart
+++ workout_app/lib/views/exercise/exercise_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workout_model/workout_model.dart';
 
 class ExerciseView extends StatefulWidget {
   const ExerciseView({super.key});
@@ -8,12 +9,59 @@ class ExerciseView extends StatefulWidget {
 }
 
 class _ExerciseViewState extends State<ExerciseView> {
+  var exerciseFuture = ExerciseRepository.instance.list();
+
+  void updateList() {
+    setState(() {
+      exerciseFuture = ExerciseRepository.instance.list();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Exercise view'),
-      ),
+    return FutureBuilder(
+      future: exerciseFuture,
+      builder: ((context, snapshot) {
+        final result = snapshot.data;
+
+        if (result != null) {
+          return Scaffold(
+            body: ListView.builder(
+              itemCount: result.length,
+              itemBuilder: (context, index) {
+                final exercise = result[index];
+                return ListTile(
+                  onTap: () {}, // Naviaget to exercise detail view
+                  title: Text(exercise.name),
+                  subtitle: Text(exercise.description),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () async {
+                      await ExerciseRepository.instance.delete(exercise.id);
+                      updateList();
+                    },
+                  ),
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                // TODO: Open modal to create exercise
+                await ExerciseRepository.instance.create(Exercise(
+                    name: 'Bänkpress', description: 'Bänka för faaan'));
+                updateList();
+              },
+              child: const Icon(Icons.add),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Error"),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      }),
     );
   }
 }
