2d37b8e6d8b97d3a95f000fca5ee2892842e9845
Emil Noren <stratosfloor@hotmail.com>
2023-11-20 16:55:53 +0100

    Add instances of repositories

--- workout_app/lib/main.dart
+++ workout_app/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:workout_app/views/exercise/exercise_view.dart';
 import 'package:workout_app/views/workout/workout_view.dart';
+import 'package:workout_model/workout_model.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final ExerciseDescriptionRepository exerciseDescriptionRepository =
+      ExerciseDescriptionRepository.instance;
+  final ExerciseRepository exerciseRepository = ExerciseRepository.instance;
+  final WorkoutRepository workoutRepository = WorkoutRepository.instance;
+  final directory = await getApplicationCacheDirectory();
+
+  await exerciseDescriptionRepository.initalize(filePath: directory.path);
+  await exerciseRepository.initalize(filePath: directory.path);
+  await workoutRepository.initalize(filePath: directory.path);
 
   runApp(
     MaterialApp(
