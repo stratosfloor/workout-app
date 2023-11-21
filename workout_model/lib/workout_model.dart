19334b803af2c45988e105771fa4443e512106ad
Emil Noren <stratosfloor@hotmail.com>
2023-11-21 09:32:10 +0100

    Export all models, repos and manager

--- workout_model/lib/workout_model.dart
+++ workout_model/lib/workout_model.dart
@@ -4,11 +4,14 @@
 library;
 
 export 'src/models/exercise_model.dart';
+export 'src/models/exercise_description_model.dart';
 export 'src/models/workout_model.dart';
 
 export 'src/workout_model_base.dart';
 export 'src/repositories/exercise_description_repository.dart';
 export 'src/repositories/exercise_repository.dart';
 export 'src/repositories/workout_repository.dart';
+
+export 'src/managers/workout_manager.dart';
 
 // TODO: Export any libraries intended for clients of this package.
